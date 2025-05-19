706c26bb635f4faacea2446506102859f7e2b5eb
ldbrian <ldbrian@163.com>
2025-05-19 13:37:22 +0800

    Create math_utils.py

---
+++ src/math_utils.py
@@ -0,0 +1,11 @@
+def add(a: float, b: float) -> float:
+    """计算两个数字的和
+    
+    Args:
+        a (float): 第一个加数
+        b (float): 第二个加数
+        
+    Returns:
+        float: 两数之和
+    """
+    return a + b
